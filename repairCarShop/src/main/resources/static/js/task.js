0926c877431a9aff0224e3aa7433e2476031fd1b
PavPetrov <pafkata@gbg.bg>
2023-12-03 13:55:09 +0200

    Added properly images amd text to tasks cards

--- repairCarShop/src/main/resources/static/js/task.js
+++ repairCarShop/src/main/resources/static/js/task.js
@@ -1,6 +1,22 @@
 const APPROVE_URL = 'http://localhost:8080/tasks/api/approve/'
 const GET_URL = 'http://localhost:8080/tasks/api'
 
+const CATEGORY_IMG = {
+    suspension: "/images/suspension.png",
+    body: "/images/body-repair.png",
+    tire: "/images/tire-repair.png",
+    engine: "/images/engine.png"
+}
+const CATEGORY_TEXT = {
+    suspension: "Suspension repair",
+    body: "Painting / Body",
+    tire: "Tires repair",
+    engine: "Engine repair"
+}
+
+
+//TODO REFACTOR Functionality in modules
+
 async function getAllTasks(url) {
     const response = await fetch(url);
     return response.json();
@@ -30,39 +46,47 @@ async function render(res) {
 
     res.forEach(res => {
         let isApproved = res.approved;
+        let repairCategory =  getRepairCategoryData(res.repairCategory);
 
+        console.log(repairCategory)
         let divBox = createElement("div", "box");
 
         let divImgBox = createElement("div", "img-box");
 
         let imgElem = createElement("img");
-        imgElem.src = "/images/body-repair.png"; // TODO different images for category
+        imgElem.src = repairCategory.imgSrc;
         divImgBox.appendChild(imgElem);
 
         let divDetailBox = createElement("div", "detail-box");
 
         let hElem = createElement("h5");
-        hElem.textContent = "Painting / Body" // TODO get text for category
+        hElem.textContent = repairCategory.text;
         divDetailBox.appendChild(hElem);
 
 
         let tableElem = createTable(res);
         divDetailBox.appendChild(tableElem);
 
-        let aLink = createElement("a");
-        aLink.href = "";
-        aLink.textContent = "Approve link"
-        aLink.addEventListener('click', approve.bind(null, res.id));
 
-        divDetailBox.appendChild(aLink);
 
 
         divBox.appendChild(divImgBox);
         divBox.appendChild(divDetailBox);
 
         if (isApproved) {
+            let pElemCreated = createElement("p");
+            pElemCreated.textContent ="Created on - " + res.created;
+            divDetailBox.appendChild(pElemCreated)
             divApproved.appendChild(divBox);
+
         } else {
+
+            let aLink = createElement("a");
+            aLink.href = "";
+            aLink.textContent = "Approve"
+            aLink.addEventListener('click', approve.bind(null, res.id));
+
+            divDetailBox.appendChild(aLink);
             divToApprove.appendChild(divBox);
         }
     })
@@ -138,19 +162,45 @@ function createTable(res) {
 
 }
 
-function  approve(id, event) {
+function approve(id, event) {
 
     event.preventDefault();
 
 
-        (async () => {
-            await fetch(APPROVE_URL + id, {
-                method: 'PATCH'
-            })
-            getAllTasks(GET_URL).then(async (response) => {
-                await render(response)
-            });
-        })();
+    (async () => {
+        await fetch(APPROVE_URL + id, {
+            method: 'PATCH'
+        })
+        getAllTasks(GET_URL).then(async (response) => {
+            await render(response)
+        });
+    })();
 
+}
 
+function getRepairCategoryData(repairCategory) {
+    let category = {
+        imgSrc:"",
+        text:""
+    };
+
+    switch (repairCategory) {
+        case "SUSPENSION":
+            category.imgSrc = CATEGORY_IMG.suspension
+            category.text = CATEGORY_TEXT.suspension;
+            break;
+        case "TIRES":
+            category.imgSrc = CATEGORY_IMG.tire
+            category.text = CATEGORY_TEXT.tire;
+            break;
+        case "ENGINE":
+            category.imgSrc = CATEGORY_IMG.engine
+            category.text = CATEGORY_TEXT.engine;
+            break;
+        case "BODY":
+            category.imgSrc = CATEGORY_IMG.body
+            category.text = CATEGORY_TEXT.body;
+            break;
+    }
+    return category;
 }
