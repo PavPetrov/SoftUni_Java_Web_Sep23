329c0a7ea737822f1f3d4d6e244c45ec9b0aa6f8
PavPetrov <pafkata@gbg.bg>
2023-12-17 14:47:21 +0200

    Scheduler to clean up config_log file if is more then $ lines

--- repairCarShop/src/main/java/org/softuni/repairShop/RepairShopApplication.java
+++ repairCarShop/src/main/java/org/softuni/repairShop/RepairShopApplication.java
@@ -2,7 +2,9 @@ package org.softuni.repairShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RepairShopApplication {
 
